73aa800077f55818eadbd65a1bfd3e5264a9b578
chasu1228 <chasu1228@users.noreply.github.com>
2018-06-24 18:35:53 +0900

    Add files via upload

---
+++ MoveObject.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObject : MonoBehaviour, IGameObject {
+
+    [SerializeField]
+    private float _startPositionX = 0.112f;
+    [SerializeField]
+    private float _endPositionX = -0.101f;
+
+	virtual public void GameUpdate () {
+        Move();
+	}
+		
+    private void Move()
+    {
+        Vector3 result = transform.position;
+		result.x -= Manager.Instance.Speed;
+		if (result.x < _endPositionX)
+        {
+            FinishEndPosition();
+        }
+        else
+        {
+			transform.position = result;
+        }
+    }
+
+    virtual protected void FinishEndPosition()
+    {
+		Vector3 result = transform.position;
+		result.x = _startPositionX;
+		transform.position = result;
+    }
+}
